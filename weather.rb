b45608275c78e78e118bc7ff3cb375b9f2e919e9
Margaret R. Moore <Margaret.Rita.Moore@gmail.com>
2014-08-02 12:07:29 -0400

    dding all the files for my weather app

---
+++ weather.rb
@@ -0,0 +1,29 @@
+require "sinatra"
+require "yahoo_weatherman"
+
+def get_weather(post)
+client = Weatherman::Client.new
+user_woeid = client.lookup_by_location(post)
+user_woeid.condition["text"]
+end
+
+get '/' do
+    erb :home
+end
+
+post '/weather' do
+    post = params[:post]["location"]
+    @weather = get_weather(post)
+    "#{@weather}"
+   
+   if (@weather == "Sunny")
+       erb:sunny
+       elsif (@weather == "Cloudy")
+       erb:cloudy
+       elsif (@weather == "Snowy")
+       erb:snowy
+       else
+       erb:default
+   end
+end
+
